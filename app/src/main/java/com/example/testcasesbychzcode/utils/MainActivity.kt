dd85db120593580bd69cd40e112cef81e06c6104
Snehal Sonawale222 <63513918+snehalsonawale@users.noreply.github.com>
2024-03-06 23:16:21 +0530

    data class added

--- app/src/main/java/com/example/testcasesbychzcode/utils/MainActivity.kt
+++ app/src/main/java/com/example/testcasesbychzcode/utils/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.testcasesbychzcode.utils
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testcasesbychzcode.utils.ui.theme.TestCasesByChzCodeTheme
 
 class MainActivity : ComponentActivity() {
@@ -88,6 +97,49 @@ fun PersonList(people: List<Person>) {
         items(people.size) {index ->
             val person = people[index]
             Text(text = "Name: ${person.name}, Age: ${person.age}")
+        }
+    }
+}
+
+private val countryList =
+    mutableListOf("India", "Pakistan", "China", "United States")
+
+data class FruitModel(val name:String, val image : Int)
+
+private val fruitsList = mutableListOf<FruitModel>()
+/*fruitsList.add(FruitModel("Apple", R.drawable.apple))
+fruitsList.add(FruitModel("Orange", R.drawable.orange))
+fruitsList.add(FruitModel("Banana", R.drawable.banana))
+fruitsList.add(FruitModel("Strawberry", R.drawable.strawberry))
+fruitsList.add(FruitModel("Mango", R.drawable.mango))*/
+
+@Composable
+fun ItemView(index: Int, selected: Boolean, onClick: (Int) -> Unit){
+    Text(
+        text = "Item $index",
+        modifier = Modifier
+            .clickable {
+                onClick.invoke(index)
+            }
+            .background(Color.Transparent)
+            .fillMaxWidth()
+            .padding(12.dp)
+    )
+}
+
+@Composable
+fun LazyColumnWithSelection(){
+    var selectedIndex by remember { mutableStateOf(0) }
+    val onItemClick = { index: Int -> selectedIndex = index}
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+    ){
+        items(100){ index ->
+            ItemView(
+                index = index,
+                selected = selectedIndex == index,
+                onClick = onItemClick
+            )
         }
     }
 }
